602bb51739d8ed750ca12de9d77925b7835d24c9
Climax Mba <climaxium150@gmail.com>
2024-07-31 20:11:33 +0100

    Create MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 import { paths } from "./_lib/constants";
 import Home from "./pages/home/Home";
@@ -9,6 +10,46 @@ import Packages from "./pages/packages/Packages";
 import Favourites from "./pages/favourites/Favourites";
 
 import "./index.scss";
+
+// declare module "@mui/material/styles" {
+//   interface Theme {
+//     color: {
+//       accent: string;
+//     };
+//   }
+//   // allow configuration using `createTheme`
+//   interface ThemeOptions {
+//     color?: {
+//       accent?: string;
+//     };
+//   }
+// }
+
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+        contained: {
+          backgroundColor: "darkcyan",
+          ":hover": {
+            backgroundColor: "#006e6e",
+          },
+        },
+        outlined: {
+          borderColor: "darkcyan",
+          color: "darkcyan",
+          ":hover": {
+            backgroundColor: "#effaf",
+            borderColor: "darkcyan",
+          },
+        }
+      },
+    },
+  },
+});
 
 export default function App() {
   const router = createBrowserRouter([
@@ -23,7 +64,7 @@ export default function App() {
       children: [
         {
           path: paths.details,
-          element: <Details />
+          element: <Details />,
         },
       ],
     },
@@ -37,5 +78,9 @@ export default function App() {
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
