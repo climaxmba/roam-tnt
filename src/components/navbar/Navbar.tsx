143b8d9b17e7c1dbefbde8dfd52748d33231fe06
Climax Mba <climaxium150@gmail.com>
2024-08-07 13:11:51 +0100

    Add mobile navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import { paths } from "../../_lib/constants";
@@ -5,51 +6,112 @@ import { paths } from "../../_lib/constants";
 import styles from "./navbar.module.scss";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleMenuClick = () => setMenuOpen((val) => !val);
+
   return (
-    <nav className={styles.container}>
-      <span>Roam T&T</span>
-      <ul>
-        <li>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? styles.active : styles.link
-            }
-            to={paths.root}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? styles.active : styles.link
-            }
-            to={paths.packages}
-          >
-            Packages
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? styles.active : styles.link
-            }
-            to={paths.favourites}
-          >
-            Favourites
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? styles.active : styles.link
-            }
-            to={paths.login}
-          >
-            Login
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className={styles.container}>
+        <span>Roam T&T</span>
+        <ul>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.active : styles.link
+              }
+              to={paths.root}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.active : styles.link
+              }
+              to={paths.packages}
+            >
+              Packages
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.active : styles.link
+              }
+              to={paths.favourites}
+            >
+              Favourites
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.active : styles.link
+              }
+              to={paths.login}
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Mobile Navbar */}
+      <nav className={styles.mobileContainer}>
+        <span>Logo</span>
+        <button
+          title="Menu"
+          className={`${styles.menu} ${menuOpen && styles.menuOpen}`}
+          onClick={handleMenuClick}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <ul className={`${styles.links} ${menuOpen && styles.linksOpen}`}>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.active : styles.link
+              }
+              to={paths.root}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.active : styles.link
+              }
+              to={paths.packages}
+            >
+              Packages
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.active : styles.link
+              }
+              to={paths.favourites}
+            >
+              Favourites
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.active : styles.link
+              }
+              to={paths.login}
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
